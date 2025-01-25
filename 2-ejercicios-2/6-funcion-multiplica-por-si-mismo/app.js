89fa8476b15b07af110795b6d43238683cb37970
Javier Morales <j.morales.labarca@gmail.com>
2025-01-25 13:33:17 -0300

    actualiza comentario

--- 2-ejercicios-2/6-funcion-multiplica-por-si-mismo/app.js
+++ 2-ejercicios-2/6-funcion-multiplica-por-si-mismo/app.js
@@ -1,4 +1,4 @@
-//Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
+// Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
 
 function ingresarNumero (num) {
     let resultado = (num * num);
